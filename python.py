87b65502f9ed654aa9b6f1c765fb223f4831622b
Hanoof-Buchh <hanoofbuchh@gmail.com>
2025-01-13 09:26:58 +0530

    added python file

---
+++ python.py
@@ -0,0 +1,25 @@
+# WAP to calculate the tax on a given salary, where the tax rate is 10% if the salary is less than 50,000, and 20% if the salary is 50,000 or more.
+# sal = float(input("salary: "))
+# tax = sal*(0.1 if sal < 50000 else 0.2)
+# print("tax: ", tax)
+
+# WAP to input 2 numbers and print  their sum
+# a = int(input("Enter first number: "))
+# b = int(input("Enter second number: "))
+# sum = a+b
+# print("sum: ",sum)
+
+# WAP to input side of a square & print its area
+# side = float(input("enter the side of a square : "))
+# print("area: ",side**2)
+
+#  WAP to input two floating point numbers & print their average
+# First_num = float(input("Enter the first number: "))
+# Second_num = float(input("Enter the second number: "))
+# Average = (First_num + Second_num)/2
+# print("Average is: ",Average)
+
+# WAP to input 2 int numbers, a and b. Print true if a is greater than or equal to b. If not print false.
+# a = int(input("First number: "))
+# b = int(input("Second number: "))
+# print(a>=b)
