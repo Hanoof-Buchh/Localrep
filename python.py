21c7b6510d8ccad4a6390c0ce218b66dd8ce9a73
Hanoof-Buchh <hanoofbuchh@gmail.com>
2025-01-13 14:16:37 +0530

    ADDED PRACTICE QUESTIONS OF RECURSION

--- python.py
+++ python.py
@@ -412,3 +412,53 @@
 #n = int(input("Enter number: "))
 #result = fact(n)
 #print("factorial is: ",result)
+
+# WAF to convert USD to INR.
+# def converter(usd_val):
+#     inr_val = usd_val * 86.43
+#     return inr_val
+
+# a = int(input("Enter USD: "))
+# result = converter(a)
+# print(a,"USD =",result,"INR")
+
+# def number(num):
+#     if num % 2 ==0:
+#         return"EVEN"
+#     else:
+#         return"ODD"
+    
+# n = int(input("enter number: "))
+# print("the number is",number(n))
+
+# def show(n):
+#     if n==0:
+#         return
+#     print(n)
+#     show(n-1)
+
+# show(5)
+
+# # write a recursive function to find the factorial of a number
+# def fact(n):
+#     if (n==1 or n==0):
+#         return 1
+#     return fact (n-1) * n
+
+# print(fact(4))
+
+# Write a recursive function to calculate the sum of first n natural numbers.
+# def sum(n):
+#     if(n==0):
+#         return 0
+#     return sum(n-1)+ n
+
+# print(sum(5))
+def print_list(list,idx=0):
+    if (idx == len(list)):
+        return
+    print(list[idx])
+    print_list(list,idx+1)
+
+fruits = ["mango","grapes","banana","apple","guava","orange","peach","lichi"]
+print_list(fruits)
