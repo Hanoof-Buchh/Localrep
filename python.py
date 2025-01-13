fea69923104101761257005f65c612a3cab77016
Hanoof-Buchh <hanoofbuchh@gmail.com>
2025-01-13 14:18:27 +0530

    Update python.py

--- python.py
+++ python.py
@@ -454,11 +454,11 @@
 #     return sum(n-1)+ n
 
 # print(sum(5))
-def print_list(list,idx=0):
-    if (idx == len(list)):
-        return
-    print(list[idx])
-    print_list(list,idx+1)
+#def print_list(list,idx=0):
+#    if (idx == len(list)):
+#        return
+#    print(list[idx])
+#    print_list(list,idx+1)
 
-fruits = ["mango","grapes","banana","apple","guava","orange","peach","lichi"]
-print_list(fruits)
+#fruits = ["mango","grapes","banana","apple","guava","orange","peach","lichi"]
+#print_list(fruits)
